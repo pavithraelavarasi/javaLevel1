cf9cb0f5e9342177da11920e64ca2a120bfe25f5
pavithraelavarasi <pavithraelavarasi@gmail.com>
2024-03-10 18:29:09 +0530

    numberPower.java

---
+++ level1/numberPower.java
@@ -0,0 +1,12 @@
+//8. Write a Java program that reads a number and display the square, cube, and fourth power.
+
+public class numberPower {
+	public static void main (String args[])
+   {
+	 int num = 12;
+	 int square = num*num;
+	 int cube = num*num*num;
+	 int fourthPower = num*num*num*num;
+	   System.out.println ("Square: " + square + "Cube:" + cube + "Fourth Power:" + fourthPower );
+   }
+}
